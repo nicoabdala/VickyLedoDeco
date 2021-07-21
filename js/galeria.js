8ba9275d9ae5242174e071d9fde4987ae235ba13
Nico <nicoabdala722@icloud.com>
2021-07-21 19:39:05 -0300

    js arreglado

--- js/galeria.js
+++ js/galeria.js
@@ -4,14 +4,14 @@ var selectedClass = "";
 $(".filter").click(function(){
 selectedClass = $(this).attr("data-rel");
 
-$("#gallery").fadeTo(100, 0.1);
+$(".gallery").fadeTo(100, 0.1);
 
-$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+$(".gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
 setTimeout(function() {
 
 $("."+selectedClass).fadeIn().addClass('animation');
 
-$("#gallery").fadeTo(300, 1);
+$(".gallery").fadeTo(300, 1);
 }, 300);
 });
 });
